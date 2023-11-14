4769abfe72091e94f7c45634a64969c8383408c0
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-14 01:00:48 -0300

    Testa os campos obrigatorios.
    - Coloca o teste inicial de visitar a página dentro de um forEach para não reescrever com cada visita futura.
    - Testa os cinco campos obrigatórios.

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,9 +1,18 @@
 /// <reference types="Cypress" />
 
 describe('Central de atendimento ao CLiente TAT', function() {
-    it('Verifica o títuloda da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./src/index.html')
-
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('Verifica o títuloda da aplicação', function() {
+         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    it('preenche os campos pbrigtórios e envia o formulário', function() {
+        cy.get('#firstName').type('Efraim')
+        cy.get('#lastName').type('Rocha')
+        cy.get('#email').type('efra@gmail.com')
+        cy.get('#open-text-area').type('Texto teste Ok')
+        cy.get('button[type="submit"]').click()
+    })  
 })

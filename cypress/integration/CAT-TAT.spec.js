0d74857c6be35ae1e828d83f332f60a151eae400
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-14 01:58:04 -0300

    Sobreescreve o delay da função type para 0.

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -9,10 +9,11 @@ describe('Central de atendimento ao CLiente TAT', function() {
          cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
     it('preenche os campos pbrigtórios e envia o formulário', function() {
+        const longText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indn unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularis release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
         cy.get('#firstName').type('Efraim')
         cy.get('#lastName').type('Rocha')
         cy.get('#email').type('efra@gmail.com')
-        cy.get('#open-text-area').type('Texto teste Ok')
+        cy.get('#open-text-area').type(longText, { delay:0 })
         cy.get('button[type="submit"]').click()
         
         cy.get('.success').should('be.visible')
